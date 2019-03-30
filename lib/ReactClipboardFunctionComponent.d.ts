cbb3283ae6b37bf37b6693aebef9d7608960220a
icepy <xiangwenwe@foxmail.com>
2019-03-30 23:43:23 +0800

    remove lib

--- lib/ReactClipboardFunctionComponent.d.ts
+++
@@ -1,9 +0,0 @@
-import React from "react";
-interface IProps {
-    text: string;
-    onCopy?: () => void;
-    onError?: () => void;
-    children?: React.ReactNode;
-}
-export default function ReactClipboardFunctionComponent(props: IProps): JSX.Element;
-export {};
--- lib/index.d.ts
+++
@@ -1,7 +0,0 @@
-import ReactClipboardFunctionComponent from "./ReactClipboardFunctionComponent";
-import ReactClipboardComponent from "./ReactClipboardComponent";
-declare const _default: {
-    ReactClipboardFunctionComponent: typeof ReactClipboardFunctionComponent;
-    ReactClipboardComponent: typeof ReactClipboardComponent;
-};
-export default _default;
